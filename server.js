42120fab27dae1c172dd8f2d919911633bb4b138
Julia Randall <juliafrandall@gmail.com>
2017-06-05 16:40:50 -0400

    server setup

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var app = express();
+var path = require('path');
+var http = require('http').Server(app);
+
+
+app.set('port',process.env.PORT || 9000);
+var port = app.get('port');
+http.listen(port);
+console.log('Server listening on PORT', port);
+
+app.get('*', function(req, res){
+  res.sendFile(path.resolve(__dirname, '../client', 'index.html'))
+});
+
+module.exports = app;
